b669085e04ac3d444cfb2682dc3ca75c54af4a6f
Oh Seyoung <dhtpdud528@gmail.com>
2024-06-11 16:43:09 +0900

    SpawnerSystem버그 수정, 코드 정리

--- Assets/Scripts/Systems/SpawnerSystem.cs
+++ Assets/Scripts/Systems/SpawnerSystem.cs
@@ -9,30 +9,11 @@ using Unity.Transforms;
 public partial struct SpawnerSystem : ISystem
 {
     [BurstCompile]
-    public void OnCreate(ref SystemState state)
-    {
-    }
-    [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         EntityCommandBuffer ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>().CreateCommandBuffer(state.WorldUnmanaged);
-        /*foreach (var (spawnerComponent, randomDataComponent, spawnerTransformComponent) in SystemAPI.Query<RefRW<SpawnerComponent>, RefRW<RandomDataComponent>, RefRO<LocalTransform>>().WithAll<SpawnerComponent>())
-        {
-            SpawnerComponent spawnerComponentRO = spawnerComponent.ValueRO;
-            if (spawnerComponentRO.spawnedCount >= spawnerComponentRO.maxCount) return;
-            randomDataComponent.ValueRW.Random = new Random((uint)randomDataComponent.ValueRO.Random.NextInt(int.MinValue, int.MaxValue));
-            Entity spawnedEntity = ecb.Instantiate(spawnerComponentRO.spawnPrefab);
-            LocalTransform spanwerTransRO = spawnerTransformComponent.ValueRO;
-            var initTransform = new LocalTransform { Position = spanwerTransRO.Position, Rotation = spanwerTransRO.Rotation, Scale = spawnerComponentRO.isRandomSize ? randomDataComponent.ValueRO.Random.NextFloat(spawnerComponentRO.minSize, spawnerComponentRO.maxSize) : spanwerTransRO.Scale };
-            ecb.SetComponent(spawnedEntity, initTransform);
-            spawnerComponent.ValueRW.spawnedCount++;
-        }*/
-        SpawnweJob job = new SpawnweJob { parallelWriter = ecb.AsParallelWriter() };
+        SpawnweJob job = new SpawnweJob { time = SystemAPI.Time, parallelWriter = ecb.AsParallelWriter() };
         job.ScheduleParallel();
-        /*var handle = job.ScheduleParallel(state.Dependency);
-
-        handle.Complete(); //차후 최적화
-        ecb.Playback(state.EntityManager);*/
     }
     [BurstCompile]
     partial struct SpawnweJob : IJobEntity
@@ -47,12 +28,12 @@ public partial struct SpawnerSystem : ISystem
                 spawnerComponent.currentSec += time.DeltaTime;
                 return;
             }
+            spawnerComponent.currentSec = 0;
             randomDataComponent.Random = new Random((uint)randomDataComponent.Random.NextInt(int.MinValue, int.MaxValue));
             Entity spawnedEntity = parallelWriter.Instantiate(chunkIndex, spawnerComponent.spawnPrefab);
             var initTransform = new LocalTransform { Position = spawnerTransformComponent.Position, Rotation = spawnerTransformComponent.Rotation, Scale = spawnerComponent.isRandomSize ? randomDataComponent.Random.NextFloat(spawnerComponent.minSize, spawnerComponent.maxSize) : 1 };
             parallelWriter.SetComponent(chunkIndex, spawnedEntity, initTransform);
             spawnerComponent.spawnedCount++;
-            spawnerComponent.currentSec = 0;
         }
     }
 }
