e2a7732b149a6a3797258c76c4261d1890dbb69f
Oh Seyoung <dhtpdud528@gmail.com>
2024-06-14 18:38:45 +0900

    설정 AoS와 통일

--- Assets/Scripts/Systems/AnimationControlSystem.cs
+++ Assets/Scripts/Systems/AnimationControlSystem.cs
@@ -7,40 +7,41 @@ using Unity.Entities;
 [UpdateBefore(typeof(SpriteUVAnimationSystem))]
 public partial struct AnimationControlSystem : ISystem
 {
-    /*private struct SystemData : IComponentData
+    private struct SystemData : IComponentData
     {
         public EntityQuery MovableQuery;
-    }*/
+    }
 
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
-        //var systemData = new SystemData();
+        var systemData = new SystemData();
         var queryBuilder = new EntityQueryBuilder(Allocator.Temp)
             .WithAll<PeepoComponent>()
             .WithAspect<AnimatorAspect>()
             .WithOptions(EntityQueryOptions.IgnoreComponentEnabledState);
         var movableQuery = state.GetEntityQuery(queryBuilder);
         movableQuery.AddChangedVersionFilter(ComponentType.ReadOnly<PeepoComponent>());
-        /*systemData.MovableQuery = movableQuery;
+        systemData.MovableQuery = movableQuery;
 
-        _ = state.EntityManager.AddComponentData(state.SystemHandle, systemData);*/
+        _ = state.EntityManager.AddComponentData(state.SystemHandle, systemData);
 
         queryBuilder.Dispose();
     }
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        //var systemData = SystemAPI.GetComponent<SystemData>(state.SystemHandle);
+        var systemData = SystemAPI.GetComponent<SystemData>(state.SystemHandle);
         if (!SystemAPI.TryGetSingleton<AnimationSettings>(out var animationSettings))
             return;
         var time = SystemAPI.Time.ElapsedTime;
 
-        new ChangeAnimationJob
+        var animationSwitchJob = new ChangeAnimationJob
         {
             AnimationSettings = animationSettings,
             Time = time
-        }.ScheduleParallel();
+        };
+        state.Dependency = animationSwitchJob.ScheduleParallelByRef(systemData.MovableQuery, state.Dependency);
     }
 
     [BurstCompile]
