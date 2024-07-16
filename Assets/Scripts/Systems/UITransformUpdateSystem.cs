440aabf9b93ad30315b4af0ebd2e50ad6626ae3b
Oh Seyoung <dhtpdud528@gmail.com>
2024-07-16 11:49:21 +0900

    채팅 말풍선 UI 위치값 Clamp 지정

--- Assets/Scripts/Systems/UITransformUpdateSystem.cs
+++ Assets/Scripts/Systems/UITransformUpdateSystem.cs
@@ -1,21 +1,31 @@
 using Cysharp.Threading.Tasks;
+using OSY;
+using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
+using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
 
-public partial struct UITransformUpdateSystem : ISystem
+public partial struct UITransformUpdateSystem : ISystem, ISystemStartStop
 {
     public JobHandle eventDepedency;
+    float2 topRightScreenPoint;
+    public void OnStartRunning(ref SystemState state)
+    {
+        topRightScreenPoint = new float2(Screen.width, Screen.height);
+    }
+
     public void OnUpdate(ref SystemState state)
     {
         if (GameManager.instance.chatBubbleUICanvasTransform.gameObject.activeInHierarchy)
-            new UpdateChatBubbleHUDJob().ScheduleParallel();
+            new UpdateChatBubbleHUDJob { topRightScreenPoint = this.topRightScreenPoint }.ScheduleParallel();
         if (GameManager.instance.nameTagUICanvasTransform.gameObject.activeInHierarchy)
-            new UpdateNameTagHUDJob().ScheduleParallel(state.Dependency).Complete();
+            new UpdateNameTagHUDJob { topRightScreenPoint = this.topRightScreenPoint }.ScheduleParallel(state.Dependency).Complete();
     }
     public partial struct UpdateChatBubbleHUDJob : IJobEntity
     {
+        [ReadOnly] public float2 topRightScreenPoint;
         public void Execute(in PeepoComponent peepo, in LocalTransform localTransform, in HashIDComponent hash)
         {
             if (GameManager.instance.viewerInfos != null)
@@ -26,17 +36,23 @@ public partial struct UITransformUpdateSystem : ISystem
         }
         public void UnitaskExecute(PeepoComponent peepo, LocalTransform localTransform, HashIDComponent hash)
         {
+            float2 maxVal = topRightScreenPoint;
             UniTask.RunOnThreadPool(async () =>
             {
                 await UniTask.SwitchToMainThread();
                 if (GameManager.instance.viewerInfos.ContainsKey(hash.ID))
                 {
-                    Transform bubbleTransform = GameManager.instance.viewerInfos[hash.ID]?.chatBubbleObjects?.transform;
+                    RectTransform bubbleTransform = (RectTransform)GameManager.instance.viewerInfos[hash.ID]?.chatBubbleObjects?.transform;
                     if (bubbleTransform != null)
                     {
-                        var targetPosition = GameManager.instance.mainCam.WorldToScreenPoint(localTransform.Position, Camera.MonoOrStereoscopicEye.Mono);
+                        Vector2 targetPosition = GameManager.instance.mainCam.WorldToScreenPoint(localTransform.Position, Camera.MonoOrStereoscopicEye.Mono);
                         targetPosition.y += 80;
-                        bubbleTransform.localPosition = targetPosition;
+                        float MinX = -maxVal.x + bubbleTransform.rect.width / 2;
+                        float MaxX = maxVal.x - bubbleTransform.rect.width / 2;
+                        float MinY = -maxVal.y;
+                        float MaxY = maxVal.y - bubbleTransform.rect.height;
+                        bubbleTransform.localPosition = (Vector2)math.clamp(targetPosition, new float2(MinX, MinY), new float2(MaxX, MaxY));
+                        //bubbleTransform.localPosition = targetPosition;
                     }
                 }
                 //new TransformJob { targetPosition = localTransform.Position }.Schedule(bubbleTransform);
@@ -45,6 +61,7 @@ public partial struct UITransformUpdateSystem : ISystem
     }
     public partial struct UpdateNameTagHUDJob : IJobEntity
     {
+        [ReadOnly] public float2 topRightScreenPoint;
         public void Execute(in PeepoComponent peepo, in LocalTransform localTransform, in HashIDComponent hash)
         {
             if (GameManager.instance.viewerInfos != null)
@@ -65,5 +82,9 @@ public partial struct UITransformUpdateSystem : ISystem
                 //new TransformJob { targetPosition = localTransform.Position }.Schedule(bubbleTransform);
             }, true, GameManager.instance.destroyCancellationToken).Forget();
         }
+    }
+
+    public void OnStopRunning(ref SystemState state)
+    {
     }
 }
