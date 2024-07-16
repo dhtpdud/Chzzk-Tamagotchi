5e68b138ec40e581b136c6d1b2228ad3daa604e7
Oh Seyoung <dhtpdud528@gmail.com>
2024-07-16 18:29:27 +0900

    물리 최적화

--- Assets/Scripts/Systems/Physic2DSystem.cs
+++ Assets/Scripts/Systems/Physic2DSystem.cs
@@ -1,12 +1,76 @@
 using Unity.Burst;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
 using Unity.Physics;
+using Unity.Physics.Systems;
 using Unity.Transforms;
-
+/*
 [BurstCompile]
+[UpdateInGroup(typeof(PhysicsSystemGroup))]
+[UpdateAfter(typeof(PhysicsSimulationGroup))]
+[UpdateBefore(typeof(ExportPhysicsWorld))]
+[RequireMatchingQueriesForUpdate]
+public partial struct ConstrainPhysicsTo2D : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<PhysicsWorldSingleton>();
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        ref PhysicsWorldSingleton physics = ref SystemAPI.GetSingletonRW<PhysicsWorldSingleton>().ValueRW;
+
+        state.Dependency = new ConstrainJob
+        {
+            Velocity = physics.MotionVelocities,
+            Data = physics.MotionDatas
+        }.Schedule(state.Dependency);
+    }
+
+    [BurstCompile]
+    private unsafe struct ConstrainJob : IJob
+    {
+        public NativeArray<MotionVelocity> Velocity;
+        public NativeArray<MotionData> Data;
+
+        public void Execute()
+        {
+            // * Fixed zero value.
+            var zC = 0;
+
+            // * Fixed float2 zero value.
+            float2 xyC = float2.zero;
+
+            var vel = (MotionVelocity*)Velocity.GetUnsafePtr();
+
+            // * Shift pointer to access Z variable of linear velocity and zero it out.
+            UnsafeUtility.MemCpyStride(&vel->LinearVelocity.z, sizeof(MotionVelocity),
+                &zC, 0, sizeof(int), Velocity.Length);
+
+            // * Shift pointer to access XY fields of angular velocity and zero them out.
+            UnsafeUtility.MemCpyStride(&vel->AngularVelocity, sizeof(MotionVelocity),
+                &xyC, 0, sizeof(float2), Velocity.Length);
+
+            var dat = (MotionData*)Data.GetUnsafePtr();
+            //float4 xyQ = new float4(0, 0, dat->BodyFromMotion.rot.value.z, dat->BodyFromMotion.rot.value.w);
+
+            // * Shift pointer to access WorldFromMotion (RigidTransform) and then the Z variable of its position.
+            UnsafeUtility.MemCpyStride(&dat->WorldFromMotion.pos.z, sizeof(MotionData),
+                &zC, 0, sizeof(int), Data.Length);
+        }
+    }
+}*/
+[BurstCompile]
+[UpdateInGroup(typeof(PhysicsSystemGroup))]
 [UpdateAfter(typeof(BeginSimulationEntityCommandBufferSystem))]
-[UpdateInGroup(typeof(SimulationSystemGroup))]
+[UpdateBefore(typeof(ExportPhysicsWorld))]
+[RequireMatchingQueriesForUpdate]
 public partial struct Physic2DSystem : ISystem
 {
     [BurstCompile]
